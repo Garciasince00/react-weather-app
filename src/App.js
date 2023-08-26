b811b8eeb9f45dad05e58045c023a1e97cd1c0d7
Garciasince00 <estelagarcia1114@gmail.com>
2023-08-26 11:10:43 +0300

    added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>React Weather App</h1>
+      <p>
+        This project was coded by Sandra Garcia and is{" "}
+        <a
+          href="https://github.com/Garciasince00/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </p>
     </div>
   );
 }

9c395f1bf6231f30cc90233a25d10bd53b5b77aa
Garciasince00 <estelagarcia1114@gmail.com>
2023-08-27 14:51:12 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>React Weather App</h1>
-      <p>
-        This project was coded by Sandra Garcia and is{" "}
-        <a
-          href="https://github.com/Garciasince00/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </p>
+      <div className="container">
+        <h1>React Weather App</h1>
+        <p>
+          This project was coded by Sandra Garcia and is{" "}
+          <a
+            href="https://github.com/Garciasince00/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </p>
+      </div>
     </div>
   );
 }

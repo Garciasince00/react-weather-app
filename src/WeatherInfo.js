5ba544cd794e1dd34a666b17ac0920067fdac3bb
Garciasince00 <estelagarcia1114@gmail.com>
2024-06-19 14:08:34 -0500

    background
    updated background

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,7 +15,7 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-4 mb-3">
         <div className="col-6">
-            <WeatherIcon code={props.data.icon} size={52}/>
+          <WeatherIcon code={props.data.icon} size={52} />
           <WeatherTemperature celsius={props.data.temperature} />
         </div>
         <div className="col-6">
